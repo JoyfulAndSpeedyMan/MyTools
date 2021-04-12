3d9be5f49358b1c47a376e4c1c8801554269318f
zhaoshipin <905991211@qq.com>
2021-04-12 19:16:49 +0800

    暂存

--- src/main/java/tools/SQLFormat.java
+++ src/main/java/tools/SQLFormat.java
@@ -7,17 +7,25 @@ import java.util.Scanner;
  * 格式化sql为：
  * sql.append("..............");
  * 格式
+ *
  * @author zhapshipin
  */
 public class SQLFormat {
     public static void main(String[] args) {
+        new SQLFormat().start();
+    }
+
+    public void start() {
+        boolean generateVar = true;
+        String varName = "sql";
+        int lineMaxLen = 120;
         Scanner scan = new Scanner(System.in);
         ArrayList<String> sqlLines = new ArrayList<>(20);
         String line;
         int max = Integer.MIN_VALUE;
         while (true) {
             line = scan.nextLine();
-            line = line.replace("\t","    ");
+            line = line.replace("\t", "    ");
             if (line.trim().isEmpty()) {
                 break;
             }
@@ -26,13 +34,63 @@ public class SQLFormat {
                 max = line.length();
             }
         }
-        String format = "%s.append(\"%-" + (max + 3) + "s\");";
+        lineMaxLen -= varName.length() + ".append(\"".length() + "\");".length();
+        max = Math.min(max + 3, lineMaxLen);
+        String format = "%s.append(\"%-" + (max) + "s\");\n";
         StringBuilder sql = new StringBuilder();
-        for (String sqlLine : sqlLines) {
-            String sql1 = String.format(format, "sql", sqlLine);
-            sql.append(sql1);
-            sql.append("\n");
+        if (generateVar) {
+            sql.append("StringBuilder ");
+            sql.append(varName);
+            sql.append(" = new StringBuilder();\n");
         }
+        for (String sqlLine : sqlLines) {
+            int len = sqlLine.length();
+            int spiltLineNum;
+            if ((spiltLineNum = (len / lineMaxLen)) > 0) {
+                int spaceOffset = findNotBlankChar(sqlLine);
+                if (spaceOffset > lineMaxLen) {
+                    spaceOffset = 0;
+                }
+                String head = sqlLine.substring(spaceOffset, lineMaxLen);
+                sql.append(String.format(format, varName, head));
+                String s = sqlLine.substring(lineMaxLen + 1);
+                String[] split = splitByMaxLen(s, lineMaxLen - spaceOffset + 1);
+                String splitFormat = "%s.append(\"%" + (max + 3) + "s\");\n";
+                for (String ss : split) {
+                    sql.append(String.format(splitFormat, varName, ss));
+                }
+            } else {
+                String sql1 = String.format(format, varName, sqlLine);
+                sql.append(sql1);
+            }
+        }
+
         System.out.println(sql.toString());
+    }
+
+    public String[] splitByMaxLen(String line, int maxLen) {
+        int len = line.length();
+        if (len <= maxLen) {
+            return new String[]{line};
+        }
+        int n = len / maxLen;
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            int start = i * maxLen;
+            int end = start + maxLen;
+            result[i] = line.substring(start,end);
+        }
+        return result;
+    }
+
+    public int findNotBlankChar(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (!Character.isSpaceChar(c)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
